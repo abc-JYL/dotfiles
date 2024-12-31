0f1ef026e42c5ce62573ea7c609836492e8cbf5d
Jeremy Low <jeremylow823@outlook.com>
2024-12-31 19:11:39 +0800

    Small tweeking

--- .config/nvim/lua/plugins_config/lsp_config.lua
+++ .config/nvim/lua/plugins_config/lsp_config.lua
@@ -1,17 +1,11 @@
 return {
     "neovim/nvim-lspconfig",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
 
     config = function()
         local lspconfig = require("lspconfig")
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-        lspconfig.clangd.setup({
-            capabilities = capabilities
-        })
-        lspconfig.gopls.setup({
-            capabilities = capabilities
-        })
+        lspconfig.clangd.setup({})
+        lspconfig.gopls.setup({})
     end
 }
 
